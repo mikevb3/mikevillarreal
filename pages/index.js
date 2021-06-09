5e2109a807156557fdbe0a202203d8f3bec5b38e
Miguel Villarreal <mikevb3@gmail.com>
2021-06-08 19:19:50 -0500

    updated index

--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1>Mike</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
